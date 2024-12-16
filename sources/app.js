5cbd17adaab8ad9712141700a13215fab7e8f777
Christian Angel <christianangel94@gmail.com>
2024-12-16 16:06:34 -0300

    Actualizando JS, vacie el div productos de forma manual y comienzo a crear la funcion que agregue los productos de manera dinamica

--- sources/app.js
+++ sources/app.js
@@ -6,9 +6,37 @@ function iniciarApp(){
     cargarMain();
 }
 
-function cargarMain(){
-    var productos = document.querySelectorAll('.contenedor-productos');
-    console.log(productos);
-    productos[0].remove();
-    productos[1].remove();
+async function cargarMain(){
+
+    try{
+        const resultado = await fetch('https://fakestoreapi.com/products/category/electronics');
+        const productos = await resultado.json();
+        console.log(productos);
+
+        const contenedorProductos = document.querySelector('#contenedor-productos')
+
+        productos.forEach(producto => {
+            //creo el div card
+
+            //imagen
+
+            //div cuerpo de la card
+
+            //titulo h5
+
+            //p-precio producto
+
+            //p-detalle prpoducto
+
+            //boton de compra (agregar al carrito) con una funcion para agregar el producto al carrito
+
+            //reseña
+
+
+        });
+
+    }catch (error){
+        console.log(`Ah ocurrido un error: ${error}`)
+    }
+    
 }
