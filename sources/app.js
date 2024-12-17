907fd4c5d5399c69b8aa61a321288c5fe7c003b2
Christian Angel <christianangel94@gmail.com>
2024-12-16 23:01:39 -0300

    Finalizando el conenedor de productos con los datos de la API

--- sources/app.js
+++ sources/app.js
@@ -16,23 +16,58 @@ async function cargarMain(){
         const contenedorProductos = document.querySelector('#contenedor-productos')
 
         productos.forEach(producto => {
+            //Hago destructuring de los atributos:
+            var {title, price, description, image} = producto;
+            
             //creo el div card
-
-            //imagen
+            const card = document.createElement('DIV');
+            card.classList.add('card');
+            card.style= 'width: 26rem;';
+            
+            //imagen del producto
+            const imagen = document.createElement('IMG');
+            imagen.src = `${image}`;
+            imagen.classList.add('card-img-top');
+            imagen.name= `${title}`;
+            imagen.alt = `${title}`;
 
             //div cuerpo de la card
+            const bodyCard = document.createElement('DIV');
+            bodyCard.classList.add('card-body');
 
             //titulo h5
+            const nombre = document.createElement('H5');
+            nombre.textContent = `${title}`;
+            nombre.classList.add('card-title');
 
             //p-precio producto
+            const precio = document.createElement('P');
+            precio.classList.add('card-text');
+            //Suponinendo que el precio esta en dolares, lo multiplico por 1030 para pasarlo a un valor aproximado en pesos
+            precio.textContent = `$${price*1030}`;
 
-            //p-detalle prpoducto
+            //p-detalle producto
+            const detalle = document.createElement('P');
+            detalle.classList.add('card-text');
+            detalle.textContent = `${description}`;
 
             //boton de compra (agregar al carrito) con una funcion para agregar el producto al carrito
-
-            //reseña
-
-
+            const botonComprar = document.createElement('A');
+            botonComprar.classList.add('btn');
+            botonComprar.classList.add('btn-primary')
+            botonComprar.text = 'Agregar al Carrito'.toUpperCase();
+            
+            //Agrego los elementos por orden al card
+            bodyCard.appendChild(nombre);
+            bodyCard.appendChild(precio);
+            bodyCard.appendChild(detalle);
+            bodyCard.appendChild(botonComprar);
+            
+            card.appendChild(imagen);
+            card.appendChild(bodyCard);
+            
+            //Agrego la card al contenedor de productos.
+            contenedorProductos.appendChild(card);
         });
 
     }catch (error){
